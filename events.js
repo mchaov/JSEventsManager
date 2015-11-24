bc48b6bc44d73653d2be4d6ac7697605339f6845
Martin Chaov <martin@mchaov.net>
2015-11-24 17:01:45 +0200

    Object.prototype methods are no longer needed
    - updated documentation
    - removed forgotten "console.log"

--- events.js
+++ events.js
@@ -25,7 +25,7 @@
  document.getElementById('myElement').hasEvent(EVENT NAME);  -> HANDLER or false
  document.getElementById('myElement').detach(EVENT NAME);    -> detaches the requested event
  document.getElementById('myElement').detach();              -> detaches all events
- document.getElementById('myElement').events                 -> returns array of all attached events if any
+ document.getElementById('myElement').events                 -> returns object with attached events as properties
  document.getElementById('myElement').trigger(EVENT NAME)    -> calls the handler
 
  //inside the events handler
@@ -52,208 +52,168 @@
  */
 
 var UIEvent = ( function() {
-        'use strict';
+	'use strict';
+	
+	function detachEvent(name) {
+		var i, ev, type, handler, useCapture;
 
-        // Empty function
-        function noop() {
+		//detach all events if no event specified
+		if (name === undefined || name === '') {
+			
+			for(i in this.eventsList) {
+				ev = this.eventsList[i];
+				type = ev.type;
+				handler = ev.handler;
+				useCapture = ev.useCapture;
+				this.removeEventListener(type, handler, useCapture);
+			}
+			this.eventsList = {};
+
+		//check for and detach if event is attached
+		} else if (this.hasEvent(name)) {
+			ev = this.hasEvent(name);
+			useCapture = ev.useCapture;
+			type = ev.type;
+			handler = ev.handler;
+			this.removeEventListener(type, handler, useCapture);
+			delete this.eventsList[name];
+		}
+
+		//if proper condition is not met
+		return this.eventsList;
+	}
+	
+	function hasEvent(name) {
+		return typeof this.eventsList[name] === 'object' ? this.eventsList[name] : false;
+	}
+	
+	function triggerEvent(name) {
+		var evt = this.hasEvent(name);
+		if ( typeof evt.handler === 'function') {
+			return evt.handler();
+		}
+		return false;
+	}
+	
+	function UIEvent(config) {
+		if (!config) {
 			return false;
-        }
+		}
 
-		if ( typeof Object.prototype.detach === 'undefined') {
-            // This prevents showing an error if you try to evoke the detach method of
-            // non EventObject.
-            // NOTE: Element.prototype.foo works in iOS 8.1+
-            Object.defineProperty(Object.prototype, 'detach', {
-                writable: true,
-                enumerable: false,
-                configurable: true,
-                value: noop
-            });
-        }
+		// Self-Invoking Constructor
+		// Make sure that a constructor function always behaves like one even
+		// if called without `new`.
+		if ((this instanceof UIEvent) === false) {
+			return new UIEvent(config);
+		}
 
-        if ( typeof Object.prototype.hasEvent === 'undefined') {
-            // This prevents showing an error if you try to evoke the detach method of
-            // non EventObject.
-            Object.defineProperty(Object.prototype, 'hasEvent', {
-                writable: true,
-                enumerable: false,
-                configurable: true,
-                value: noop
-            });
-        }
-		
-        if ( typeof Object.prototype.trigger === 'undefined') {
-            // This prevents showing an error if you try to evoke the detach method of
-            // non EventObject.
-            Object.defineProperty(Object.prototype, 'trigger', {
-                writable: true,
-                enumerable: false,
-                configurable: true,
-                value: noop
-            });
-        }
-		
-		function detachEvent(name) {
-	        var i, ev, type, handler, useCapture;
-	        console.log(this);
-	
-	        //detach all events if no event specified
-	        if (name === undefined || name === '') {
-	        	
-	        	for(i in this.eventsList) {
-	        		ev = this.eventsList[i];
-	                type = ev.type;
-	                handler = ev.handler;
-	                useCapture = ev.useCapture;
-	        		this.removeEventListener(type, handler, useCapture);
-	        	}
-	            this.eventsList = {};
-	
-	        //check for and detach if event is attached
-	        } else if (this.hasEvent(name)) {
-	            ev = this.hasEvent(name);
-	            useCapture = ev.useCapture;
-	            type = ev.type;
-	            handler = ev.handler;
-	            this.removeEventListener(type, handler, useCapture);
-	            delete this.eventsList[name];
-	        }
-	
-	        //if proper condition is not met
-	        return this.eventsList;
-	    }
-	    
-	    function hasEvent(name) {
-	    	return typeof this.eventsList[name] === 'object' ? this.eventsList[name] : false;
-	    }
-	    
-	    function triggerEvent(name) {
-	        var evt = this.hasEvent(name);
-	        if ( typeof evt.handler === 'function') {
-	            return evt.handler();
-	        }
-	        return false;
-	    }
-		
-        function UIEvent(config) {
-            if (!config) {
-                return false;
-            }
+		// Apply configuration
+		this.htmlRef = config.htmlRef;
 
-            // Self-Invoking Constructor
-            // Make sure that a constructor function always behaves like one even
-            // if called without `new`.
-            if ((this instanceof UIEvent) === false) {
-                return new UIEvent(config);
-            }
+		this.eventConfig = {
+			name : config.name,
+			type : config.type,
+			handler : config.handler === undefined ? false : config.handler,
+			useCapture : config.useCapture === undefined ? false : config.useCapture
+		};
 
-            // Apply configuration
-            this.htmlRef = config.htmlRef;
+		this.init();
+	}
 
-            this.eventConfig = {
-                name : config.name,
-                type : config.type,
-                handler : config.handler === undefined ? false : config.handler,
-                useCapture : config.useCapture === undefined ? false : config.useCapture
-            };
+	//main function
+	UIEvent.prototype.init = function() {
+		//if HTML element is not UI element
+		if (this.htmlRef.eventsList === undefined) {
 
-            this.init();
-        }
+			//extend model for this element with 'events'
+			Object.defineProperties(this.htmlRef, {
 
-        //main function
-        UIEvent.prototype.init = function() {
-            //if HTML element is not UI element
-            if (this.htmlRef.eventsList === undefined) {
+				//add array to store events
+				'eventsList' : {
+					writable : true,
+					enumerable : false,
+					configurable : false,
+					value : {}
+				},
 
-                //extend model for this element with 'events'
-                Object.defineProperties(this.htmlRef, {
+				//short-cut to add new or get all events
+				'events' : {
+					enumerable : false,
+					configurable : false,
 
-                    //add array to store events
-                    'eventsList' : {
-                        writable : true,
-                        enumerable : false,
-                        configurable : false,
-                        value : {}
-                    },
+					//if no value passed return all values stored
+					get : function() {
+						return this.eventsList;
+					},
 
-                    //short-cut to add new or get all events
-                    'events' : {
-                        enumerable : false,
-                        configurable : false,
+					//if value is passed, push into the array
+					set : function(e) {
+						return this.eventsList[e.name] = e;
+					}
+				},
 
-                        //if no value passed return all values stored
-                        get : function() {
-                            return this.eventsList;
-                        },
+				//trigger event by name
+				'trigger' : {
+					writable : false,
+					enumerable : false,
+					configurable : false,
+					value : triggerEvent
+				},
 
-                        //if value is passed, push into the array
-                        set : function(e) {
-                            return this.eventsList[e.name] = e;
-                        }
-                    },
+				//enables us to check if a specific event is attached by name
+				//if the event exist it returns the event, if not - false
+				'hasEvent' : {
+					writable : false,
+					enumerable : false,
+					configurable : false,
+					value : hasEvent
+				},
 
-                    //trigger event by name
-                    'trigger' : {
-                        writable : false,
-                        enumerable : false,
-                        configurable : false,
-                        value : triggerEvent
-                    },
+				//enables us to detach specific or all events
+				'detach' : {
+					writable : false,
+					enumerable : false,
+					configurable : false,
+					value : detachEvent
+				}
+			});
+		}
+		//if this is initialized event ... do nothing
+		else if (this.htmlRef.hasEvent(this.eventConfig.name)) {
+			return false;
+		}
 
-                    //enables us to check if a specific event is attached by name
-                    //if the event exist it returns the event, if not - false
-                    'hasEvent' : {
-                        writable : false,
-                        enumerable : false,
-                        configurable : false,
-                        value : hasEvent
-                    },
-
-                    //enables us to detach specific or all events
-                    'detach' : {
-                        writable : false,
-                        enumerable : false,
-                        configurable : false,
-                        value : detachEvent
-                    }
-                });
-            }
-            //if this is initialized event ... do nothing
-            else if (this.htmlRef.hasEvent(this.eventConfig.name)) {
-                return false;
-            }
-
-            //pass scope, apply event, save configuration for future uses
-            this.eventConfig.handler = this.eventConfig.handler.bind(this);
-            this.htmlRef.addEventListener(this.eventConfig.type, this.eventConfig.handler, this.eventConfig.useCapture);
-            this.htmlRef.events = this.eventConfig;
-        };
+		//pass scope, apply event, save configuration for future uses
+		this.eventConfig.handler = this.eventConfig.handler.bind(this);
+		this.htmlRef.addEventListener(this.eventConfig.type, this.eventConfig.handler, this.eventConfig.useCapture);
+		this.htmlRef.events = this.eventConfig;
+	};
 
 
-        Object.defineProperties(UIEvent.prototype, {
+	Object.defineProperties(UIEvent.prototype, {
 
-            //detach event method called from the event object stored in a variable
-            //you can also detach one event from within a handler of another event
-            'detach': {
-                writable: false,
-                enumerable: false,
-                configurable: false,
-                value: function(name){
-                	return detachEvent.call(this.htmlRef, name);
-                }
-            },
+		//detach event method called from the event object stored in a variable
+		//you can also detach one event from within a handler of another event
+		'detach': {
+			writable: false,
+			enumerable: false,
+			configurable: false,
+			value: function(name){
+				return detachEvent.call(this.htmlRef, name);
+			}
+		},
 
-            //trigger self
-            'trigger': {
-                writable: false,
-                enumerable: false,
-                configurable: false,
-                value: function(name) {
-                	return triggerEvent.call(this.htmlRef, name || this.eventConfig.name);
-                }
-            }
+		//trigger self
+		'trigger': {
+			writable: false,
+			enumerable: false,
+			configurable: false,
+			value: function(name) {
+				return triggerEvent.call(this.htmlRef, name || this.eventConfig.name);
+			}
+		}
 
-        });
+	});
 
-        return UIEvent;
-    }());
+	return UIEvent;
+}());
